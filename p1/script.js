87604955eed91db87d7b47e2d75bd3b44a2de3dd
Mitchell Tyson <mitchell.tyson@intersystems.com>
2019-10-04 13:04:16 -0400

    week 5 stuff

--- p1/script.js
+++ p1/script.js
@@ -1,7 +1,10 @@
 let app = new Vue({
     el: '#app',
     data: {
-        'started': null
+        'started': null,
+        'answer': null,
+        'factor1': null,
+        'factor2': null
     },
     methods: {
         begin() {
