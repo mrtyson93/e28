5231ef188de9181ce5cf9dd2196d44b3167763a0
Mitchell Tyson <mitchell.tyson@intersystems.com>
2019-10-07 11:16:38 -0400

    made progress on how user is doing, problem timer, and creating a new problem

--- p1/script.js
+++ p1/script.js
@@ -1,15 +1,46 @@
 let app = new Vue({
     el: '#app',
     data: {
-        'started': false,
-        'answer': null,
-        'factor1': 5,
-        'factor2': 8,
-        'time': 3,
+        started: false,
+        answer: null,
+        factor1: null,
+        factor2: null,
+        time: 3000,
+        timeLeft: null,
+        questionNumber: 10,
+        correctAnswers: 0,
+        problemTimer: null,
     },
     methods: {
         begin() {
+            this.newProblem();
             this.started = true;
+            this.timeLeft = this.time;
+            clearInterval(this.problemTimer);
+            this.problemTimer = setInterval(this.checkTime, 100);
+        },
+        checkTime() {
+            this.timeLeft -= 100;
+            //timer ran out on question
+            if (this.timeLeft == 0) {
+                this.questionNumber--;
+                this.timeLeft = this.time;
+                if (this.questionNumber == 0) {
+                    alert('ok');
+                }
+            }
+        },
+        newProblem() {
+            this.factor1 = Math.floor(Math.random() * 10);
+            this.factor2 = Math.floor(Math.random() * 10);
+        }
+    },
+    computed: {
+        roundedTime: function () {
+            return this.timeLeft / 1000;
+        },
+        incorrectAnswers: function () {
+            return 10 - this.questionNumber - this.correctAnswers;
         }
     }
 });
