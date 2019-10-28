5c64841d35cb148f732b7afed4db69cf6988699e
Mitchell Tyson <mitchell.tyson@intersystems.com>
2019-10-28 13:23:26 -0400

    converted p2 to comprehensive project

--- p2-alt/src/App.vue
+++ p2-alt/src/App.vue
@@ -1,28 +1,183 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="padleft" v-cloak>
+    <h1>
+      <u>Mutlipliation Table Practice</u>
+    </h1>
+    <game-rules></game-rules>
+
+    <div v-if="!started">
+      <level-picker @newtimepick="handleTimeChange($event)"></level-picker>
+      <button @click="begin">Begin!</button>
+    </div>
+
+    <div v-else>
+      <p>
+        Time Left:
+        <b class="time">{{ roundedTime }}</b> seconds
+      </p>
+      <div class="factors">
+        <label>&nbsp;&nbsp;</label>
+        {{ factor1 }}
+        <br />
+        <u>&times; {{ factor2 }}</u>
+        <br />
+        <input
+          class="answer"
+          v-show="!gameOver"
+          ref="answer"
+          v-model.number.trim="answer"
+          type="text"
+          @keyup.enter="answerSubmitted"
+        />
+      </div>
+      <br />
+      <button v-if="!gameOver" class="submit" @click="answerSubmitted">Submit</button>
+      <div v-else>
+        <div class="results">
+          Game over, you answered {{ correctAnswers }} out of 10 questions
+          correctly!
+        </div>
+        <div>
+          <level-picker @newtimepick="handleTimeChange($event)"></level-picker>
+          <button @click="begin">Play Again!</button>
+        </div>
+      </div>
+      <br />
+      <div v-show="!gameOver" class="padtop">
+        Questions Left:
+        <b>{{ questionNumber }}</b>
+        <br />Correct Answers:
+        <b>{{ correctAnswers }}</b>
+        <br />Incorrect Answers:
+        <b>{{ incorrectAnswers }}</b>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GameRules from "./components/GameRules.vue";
+import LevelPicker from "./components/LevelPicker.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    GameRules,
+    LevelPicker
+  },
+  data: function() {
+    return {
+      started: false,
+      answer: null,
+      factor1: null,
+      factor2: null,
+      time: 3000,
+      timeLeft: null,
+      questionNumber: 10,
+      correctAnswers: 0,
+      problemTimer: null,
+      gameOver: false
+    };
+  },
+  methods: {
+    begin() {
+      this.newProblem();
+      this.started = true;
+      this.gameOver = false;
+      this.questionNumber = 10;
+      this.correctAnswers = 0;
+      this.setCursor();
+      this.timeLeft = this.time;
+      clearInterval(this.problemTimer);
+      this.problemTimer = setInterval(this.checkTime, 100);
+    },
+    checkTime() {
+      this.timeLeft -= 100;
+      //timer ran out on question
+      if (this.timeLeft == 0) {
+        this.answerSubmitted();
+      }
+    },
+    newProblem() {
+      let temp = Math.floor(Math.random() * 10);
+      this.factor1 = "\xa0\xa0\xa0\xa0" + temp;
+      this.factor2 = Math.floor(Math.random() * 10);
+    },
+    answerSubmitted() {
+      if (this.answer == this.factor1 * this.factor2) this.correctAnswers++;
+      this.questionNumber--;
+      this.timeLeft = this.time;
+      this.setCursor();
+      if (this.questionNumber == 0) {
+        this.endGame();
+      } else this.newProblem();
+    },
+    setCursor() {
+      this.$nextTick(() => {
+        this.$refs.answer.focus();
+      });
+      this.answer = null;
+    },
+    endGame() {
+      clearInterval(this.problemTimer);
+      this.gameOver = true;
+      this.timeLeft = 0;
+    },
+    handleTimeChange: function(time) {
+      this.time = time;
+    }
+  },
+  computed: {
+    roundedTime: function() {
+      return Number.parseFloat(this.timeLeft / 1000).toFixed(1);
+    },
+    incorrectAnswers: function() {
+      return 10 - this.questionNumber - this.correctAnswers;
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+[v-cloak] {
+  display: none;
+}
+
+.padleft {
+  padding-left: 15px;
+}
+
+.padtop {
+  padding-top: 10px;
+}
+
+.factors {
+  font-weight: bold;
+  font-size: 25px;
+  font-family: Tahoma, Geneva, sans-serif;
+  position: relative;
+  left: 15px;
+}
+
+.answer {
+  width: 40px;
+  height: 25px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-size: 22px;
+}
+
+.submit {
+  position: relative;
+  left: 10px;
+}
+
+.time {
+  color: red;
+}
+
+.results {
+  font-family: Tahoma, Geneva, sans-serif;
+  font-weight: bold;
+  font-size: 17px;
 }
 </style>
---
+++ p2-alt/src/components/GameRules.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p>
+      You will have 10 basic multiplication problems to answer, and a limited time to answer each question.
+      <br />You will be scored out of these 10 problems. To begin the game, select the time per question you would like,
+      then
+      press Begin.
+      <br />The game will place your cursor in the answer box, you can submit an answer by pressing enter,
+      <br />or if time runs out, whatever is in the input box will be submitted. Good luck!
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {};
+  },
+  template: "#game-rules"
+};
+</script>
+
+<style>
+</style>
