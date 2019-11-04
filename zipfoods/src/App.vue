ed57c7093d14fc482dc6d7a2a3051b9a9a345f85
Mitchell Tyson <mitchell.tyson@intersystems.com>
2019-11-04 09:43:57 -0500

    finished part 3 week 9 videos

--- zipfoods/src/App.vue
+++ zipfoods/src/App.vue
@@ -1,20 +1,32 @@
 <template>
   <div id="app">
-    <img id="logo" alt="Vue logo" src="./assets/images/zipfoods-logo.png" />
+    <img
+      id="logo"
+      alt="Vue logo"
+      src="./assets/images/zipfoods-logo.png"
+    />
     <p>
       ZipFoods is your one-stop-shop for convenient online grocery shopping in
       the greater Boston area.
     </p>
-
-    <show-products></show-products>
+    <show-featured :products='products'></show-featured>
+    <show-products :products='products'></show-products>
   </div>
 </template>
 
 <script>
 import ShowProducts from "./components/ShowProducts.vue";
+import ShowFeatured from "./components/ShowFeatured.vue";
+import { products } from "./products.js";
+
 export default {
   name: "app",
-  components: { ShowProducts }
+  components: { ShowProducts, ShowFeatured },
+  data: function() {
+    return {
+      products: products
+    };
+  }
 };
 </script>
 
