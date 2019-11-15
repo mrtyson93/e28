ffb30ffbae7d841b372353691be821899718b8c4
Mitchell Tyson <mitchell.tyson@intersystems.com>
2019-11-15 11:27:54 -0500

    dummy api call

--- p3/src/components/pages/WeatherPage.vue
+++ p3/src/components/pages/WeatherPage.vue
@@ -1,12 +1,28 @@
 <template>
-  <div><p>Weather Page</p></div>
+  <div>
+    <p>Weather Page</p>
+    {{ products }}
+  </div>
 </template>
 
 <script>
+const axios = require('axios');
+
 export default {
   name: 'WeatherPage',
   data: function() {
-    return {};
+    return {
+      products: []
+    };
+  },
+  mounted() {
+    this.products = axios
+      .get(
+        'https://my-json-server.typicode.com/susanBuck/e28-zipfoods-api/products'
+      )
+      .then(response => {
+        this.products = response.data;
+      });
   }
 };
 </script>
