d4f154d3d39d2906eaa49f0d892d152a87bcf22d
Mitchell Tyson <mitchell.tyson@intersystems.com>
2019-12-12 15:05:18 -0500

    updated to use firebase and all 50 cities

--- p4/src/components/pages/SearchPage.vue
+++ p4/src/components/pages/SearchPage.vue
@@ -18,20 +18,29 @@
           :left="trueBool"
           :absolute="trueBool"
           @click="search"
-          >Search</v-btn
-        >
+        >Search</v-btn>
       </div>
     </span>
-    <v-dialog v-model="badSearch" max-width="290">
+    <v-dialog
+      v-model="badSearch"
+      max-width="290"
+    >
       <v-card>
-        <v-card-title class="headline grey lighten-2" primary-title>
+        <v-card-title
+          class="headline grey lighten-2"
+          primary-title
+        >
           Empty Search
         </v-card-title>
         <v-card-text>You must choose a city before searching.</v-card-text>
         <v-divider></v-divider>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn color="primary" text @click="badSearch = false">
+          <v-btn
+            color="primary"
+            text
+            @click="badSearch = false"
+          >
             Ok
           </v-btn>
         </v-card-actions>
@@ -41,7 +50,7 @@
 </template>
 
 <script>
-const axios = require('axios');
+const axios = require("axios");
 
 export default {
   data() {
@@ -59,20 +68,20 @@ export default {
   methods: {
     search: function() {
       if (this.cityState != null) {
-        localStorage.setItem('searched', true);
+        localStorage.setItem("searched", true);
 
-        let cityStateSplit = this.cityState.split(', ');
-        localStorage.setItem('city', cityStateSplit[0]);
-        localStorage.setItem('state', cityStateSplit[1]);
-        localStorage.setItem('zip', this.zips[cityStateSplit[0]]);
-        this.$router.push('/weather');
+        let cityStateSplit = this.cityState.split(", ");
+        localStorage.setItem("city", cityStateSplit[0]);
+        localStorage.setItem("state", cityStateSplit[1]);
+        localStorage.setItem("zip", this.zips[cityStateSplit[0]]);
+        this.$router.push("/weather");
       } else {
         this.badSearch = true;
       }
     },
     getZips: function() {
       this.zips = axios
-        .get('https://my-json-server.typicode.com/mrtyson93/e28-p3-cities/zips')
+        .get("https://e28-p4-mtyson.firebaseio.com/zips.json")
         .then(response => {
           this.zips = response.data;
           this.jsonResponseBool = true;
@@ -80,11 +89,11 @@ export default {
     }
   },
   mounted() {
-    if (!localStorage.getItem('searched')) {
-      localStorage.setItem('searched', false);
+    if (!localStorage.getItem("searched")) {
+      localStorage.setItem("searched", false);
     }
     this.cities = axios
-      .get('https://my-json-server.typicode.com/mrtyson93/e28-p3-cities/cities')
+      .get("https://e28-p4-mtyson.firebaseio.com/cities.json")
       .then(response => {
         this.cities = response.data;
         this.getZips();
